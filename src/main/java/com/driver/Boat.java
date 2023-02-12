065b2cacfb82c08052e2176b14c1836d13d1a594
souvikadhikary <souvik500>
2023-02-12 22:51:34 +0530

    Vehicle App

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -19,5 +19,19 @@ public class Boat implements WaterVehicle{
         return capacity;
     }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
 }
