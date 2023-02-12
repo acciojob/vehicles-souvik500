43c5b805cab616b8b69236e39f3ca7b810b503cf
souvikadhikary <souvik500>
2023-02-12 22:09:58 +0530

    Vehicle App

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -1,13 +1,23 @@
 package com.driver;
 
 public class Boat implements WaterVehicle{
+    private String name;
+    private int capacity;
+
+    public Boat(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
     @Override
     public String getVehicleName() {
-        return getVehicleName();
+        return this.name;
     }
 
     @Override
     public int getVehicleCapacity() {
-        return getVehicleCapacity();
+        return this.capacity;
     }
+
+
 }
